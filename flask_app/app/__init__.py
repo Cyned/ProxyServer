18b55112971548b979b08a0953ed385f9ace912c
cyned <cynedenis@gmail.com>
2019-03-18 23:12:22 +0200

    fix bug with source root

--- flask_app/app/__init__.py
+++ flask_app/app/__init__.py
@@ -1,11 +1,15 @@
+import sys
+
 from flask import Flask
 
-from app.logger import create_logger
+from flask_app.app.logger import create_logger
 from flask_app.config import DefaultConfig
 
 
 app = Flask(__name__)
 app.config.from_object(DefaultConfig)
+
+sys.path.append(app.config['APP_DIR'])
 
 logger = create_logger(app.config["APP_DIR"])
 logger.info('Created application')
