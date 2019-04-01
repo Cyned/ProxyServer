d6bf73d301c152f6a04877dd9ba9069d765fa50b
cyned <cynedenis@gmail.com>
2019-04-01 13:49:08 +0300

    fix relative urls

--- flask_app/app/server.py
+++ flask_app/app/server.py
@@ -1,8 +1,9 @@
 import requests
 
-from urllib.parse import urlparse
-from flask import request, Response, make_response
+from flask import request, Response
 
+
+from utils import replace_with_host, find_all_relative_urls, get_host
 from app import app, logger
 
 CHUNK_SIZE = 1024
@@ -11,49 +12,29 @@ CHUNK_SIZE = 1024
 @app.route('/', methods=["GET", "POST", "PUT", "DELETE"])
 @app.route('/<path:path>', methods=["GET", "POST", "PUT", "DELETE"])
 def google_page(path='zapretno.info/klip-haski-iuda'):
-    if not path.startswith('http'):
-        path = 'http://' + path
     logger.info(f'Fetching {path}')
-    proxy_ref = proxy_ref_info(request)
-    headers = {"Referer": f'http://{proxy_ref[0]}/{proxy_ref[1]}'} if proxy_ref else {}
-    logger.info(f'Fetching with headers: {path}, {headers}')
-    resp = requests.get(path, stream=True, params=request.args, headers=headers)
+    if 'localhost' in request.host_url:
+        if not path.startswith('http'):
+            path = 'http://' + path
+        domain, suffix = get_host(path)
+        domain = f'http://{domain}'
+    else:
+        domain, suffix = request.host_url, path
+    resp = requests.get(f'{domain}{suffix}', stream=True, params=request.args, timeout=5)
+    all_urls = find_all_relative_urls(resp.content.decode('utf-8', errors='ignore'))
+    logger.info('Urls in the web page: ' + str(all_urls))
+
+    if all_urls:
+        new_content = replace_with_host(urls=all_urls, host=domain, content=resp.content.decode(encoding='utf-8'))
+        logger.info('Get new content')
+        resp._content = new_content.encode('utf-8')
+
     logger.info(f'Got {resp.status_code} response from {path}')
     return Response(
         generate(response=resp),
-        # headers=request.headers,
     )
 
 
 def generate(response):
     for chunk in response.iter_content(CHUNK_SIZE):
         yield chunk
-
-
-def split_url(url):
-    """Splits the given URL into a tuple of (protocol, host, uri)"""
-    proto, rest = url.split(':', 1)
-    rest = rest.split('/', 1)
-    host, uri = (rest[0], rest[1]) if len(rest) == 2 else (rest[0], "")
-    return proto, host, uri
-
-
-def proxy_ref_info(req):
-    """Parses out Referer info indicating the request is from a previously proxied page.
-    For example, if:
-        Referer: http://localhost:8080/p/google.com/search?q=foo
-    then the result is:
-        ("google.com", "search?q=foo")
-    """
-    ref = req.host_url + req.full_path[1:]
-    if ref:
-        _, _, uri = split_url(ref)
-        if uri.find("/") < 0:
-            return None
-        first, rest = uri.split("/", 1)
-        # if first in "pd":
-        parts = rest.split("/", 1)
-        r = (parts[0], parts[1]) if len(parts) == 2 else (parts[0], "")
-        logger.info(f'Referred by proxy host, uri: {r[0]}, {r[1]}')
-        return r
-    return None
